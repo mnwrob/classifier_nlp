c5a3fc0bdbe37a5a4b1bdfdde8f4f6a1d6976161
Marion Wroblewitz <nicole.99mw@gmail.com>
2021-08-26 11:20:54 +0200

    finished code

--- AP_project.py
+++ AP_project.py
@@ -7,8 +7,13 @@ nltk.download('stopwords')
 import pickle
 from nltk.corpus import stopwords
 
+print("load start")
 dataset = load_files(r"/Users/marion/school/classifier_nlp/data")
+print("load complete")
+
+print("X,y start")
 X, y = dataset.data, dataset.target
+print("X,Y complete")
 
 documents = []
 
@@ -17,8 +22,14 @@ from nltk.tokenize import word_tokenize
 
 stem = WordNetLemmatizer()
 
-for sen in range(0, len(X)):
+print("len(X) = ", len(X))
 
+X = X[:1000]
+y = y[:1000]
+
+print("len(X) = ", len(X))
+
+for sen in range(0, len(X)):
     # Remove all the special characters
     document = re.sub(r'/[^a-z\d ]+/i', ' ', str(X[sen]))
 
@@ -42,6 +53,8 @@ for sen in range(0, len(X)):
     
     documents.append(document)
 
+print("finished lemmatizing")
+
 from sklearn.feature_extraction.text import CountVectorizer
 
 vectorizer = CountVectorizer()
@@ -52,6 +65,18 @@ from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
+from sklearn.ensemble import RandomForestClassifier
+
+classifier = RandomForestClassifier(n_estimators=1000, random_state=0)
+classifier.fit(X_train, y_train) 
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+print(confusion_matrix(y_test,y_pred))
+print(classification_report(y_test,y_pred))
+print(accuracy_score(y_test, y_pred))
 
 
 
