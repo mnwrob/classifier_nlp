5d9dba25503e8bd1d69a7a184e104b8266a18b1b
Marion Wroblewitz <nicole.99mw@gmail.com>
2021-09-19 11:03:01 +0200

    incorporated Lenas changes

--- AP_project.py
+++ AP_project.py
@@ -1,12 +1,18 @@
 import numpy as np
 import re
 import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet
 from sklearn import datasets
 from sklearn.datasets import load_files
-nltk.download('stopwords')
-import pickle
-from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
+# Loading data
 print("load start")
 dataset = load_files(r"/Users/marion/school/classifier_nlp/data", encoding="latin-1")
 print("load complete")
@@ -15,45 +21,33 @@ print("X,y start")
 X, y = dataset.data, dataset.target
 print("X,Y complete")
 
-
+# Optional:
+# Adjusting size of data
+print("len(X) = ", len(X))
+#X = X[:1000]
+#y = y[:1000]
 print("len(X) = ", len(X))
 
-X = X[:2000]
-y = y[:2000]
-
-print("len(X) = ", len(X))
-
-
-from nltk.tokenize import word_tokenize
-
-scripts = []
-
+# Text preprocessing
+documents = []
 for sen in range(0, len(X)):
-    # remove all new lines
-    script = re.sub(r'\\n', ' ', str(X[sen]))
+    # Removing all new lines
+    document = re.sub(r'\\n', ' ', str(X[sen]))
 
-    # convert to lowercase
-    script = script.lower()
+    # Converting to Lowercase
+    document = document.lower()
 
-    # remove all the special characters
-    script = re.sub(r'[^a-z\d\']+', ' ', script)
+    # Removing all special characters
+    document = re.sub(r'[^a-z\d\']+', ' ', document)
 
-    # substitute multiple spaces with single space
-    script = re.sub(r' +', ' ', script)
-
-    # tokenization
-    script = nltk.word_tokenize(script)
-
-    # join and append data
-    script = ' '.join(script)
+    # Substituting multiple spaces with single space
+    document = re.sub(r' +', ' ', document)
     
-    scripts.append(scripts)
+    documents.append(document)
 
-
-from nltk.corpus import wordnet
-
-# translate nltk tags to wordnet tags
-def nltk_to_wordnet(nltk_tag):
+# Needed for correct form of lemmatization:
+# Translating nltk tag to wordnet tag
+def nltk_tag_to_wordnet_tag(nltk_tag):
     if nltk_tag.startswith('J'):
         return wordnet.ADJ
     elif nltk_tag.startswith('V'):
@@ -65,56 +59,46 @@ def nltk_to_wordnet(nltk_tag):
     else:          
         return None
 
-
-from nltk.stem import WordNetLemmatizer
-
+# Lemmatizing with correct POS tags
 lemmatizer = WordNetLemmatizer()
-
 def lemmatize_sentence(sentence):
-    # tokenize sentence, find the POS tags 
+    # Tokenizing sentence and finding POS tag for each token
     nltk_tagged = nltk.pos_tag(nltk.word_tokenize(sentence))  
-    # create tuple of token and wordnet_tag
-    wordnet_tagged = map(lambda x: (x[0], nltk_to_wordnet(x[1])), nltk_tagged)
-    lemmatized_sen = []
+    # Creating tuple of token and wordnet tag
+    wordnet_tagged = map(lambda x: (x[0], nltk_tag_to_wordnet_tag(x[1])), nltk_tagged)
+    # Lemmatizing according to tag
+    lemmatized_sentence = []
     for word, tag in wordnet_tagged:
         if tag is None:
-            # if no tag, append the token without one
-            lemmatized_sen.append(word)
+            # If there is no available tag, appending token without one
+            lemmatized_sentence.append(word)
         else:        
-            # else use tag to lemmatize token
-            lemmatized_sen.append(lemmatizer.lemmatize(word, tag))
-    return " ".join(lemmatized_sen)
+            # Else using tag to lemmatize token
+            lemmatized_sentence.append(lemmatizer.lemmatize(word, tag))
+    return " ".join(lemmatized_sentence)
+
+lemmatized_documents =[]
+for document in documents:
+    lemmatized_documents.append(lemmatize_sentence(document))
 
 print("finished lemmatizing")
 
 
-from sklearn.feature_extraction.text import CountVectorizer
+# Vectorizing data
+vectorizer = CountVectorizer(max_df=0.4)
+X = vectorizer.fit_transform(lemmatized_documents).toarray()
 
-# extract features and convert to numbers
-vectorizer = CountVectorizer()
-
-X = vectorizer.fit_transform(scripts).toarray()
-
-
-from sklearn.model_selection import train_test_split
-
-# divide data into train and test sets
+# Splitting data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-
-from sklearn.ensemble import RandomForestClassifier
-
-# train model using fit and RandomForestClassifier method 
+# Training data with random forest classifier
 classifier = RandomForestClassifier(n_estimators=1000, random_state=0)
 classifier.fit(X_train, y_train) 
 
-# predict test data using predict method
+# Using classifier to predict correct label of test data
 y_pred = classifier.predict(X_test)
 
-
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
-
-# evaluate train model using sklearn.metrics
+# Printing some metrics about the model's performance
 print(confusion_matrix(y_test,y_pred))
 print(classification_report(y_test,y_pred))
 print(accuracy_score(y_test, y_pred))
